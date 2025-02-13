3cb35f632512ba0c0ed07d5b2532843e01148ab0
AbuAbudy <masfurudat@gmail.com>
2025-02-13 13:59:26 +0300

    isyourhorseshoeontheotherhoof?.cpp

--- isyourhorseshoeontheotherhoof?.cpp
+++ isyourhorseshoeontheotherhoof?.cpp
@@ -6,13 +6,11 @@ int main() {
     int s1, s2, s3, s4;
     cin >> s1 >> s2 >> s3 >> s4;
 
-    set<int> colors; // Set to store unique colors
+    set<int> colors;
     colors.insert(s1);
     colors.insert(s2);
     colors.insert(s3);
     colors.insert(s4);
-
-    // The number of horseshoes to buy is 4 minus the number of distinct colors
     cout << 4 - colors.size() << endl;
 
     return 0;
