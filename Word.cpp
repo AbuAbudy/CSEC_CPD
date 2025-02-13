6aa243b43f9ce43186d4000c81f5375d9cbe8fbf
AbuAbudy <masfurudat@gmail.com>
2025-02-13 13:58:55 +0300

    Word.cpp

--- Word.cpp
+++ Word.cpp
@@ -8,7 +8,6 @@ int main() {
     
     int upper_count = 0, lower_count = 0;
     
-    // Count the uppercase and lowercase letters
     for (char c : s) {
         if (isupper(c)) {
             upper_count++;
@@ -17,20 +16,16 @@ int main() {
         }
     }
     
-    // Convert based on the count comparison
     if (upper_count > lower_count) {
-        // Convert to uppercase
         for (char &c : s) {
             c = toupper(c);
         }
     } else {
-        // Convert to lowercase
         for (char &c : s) {
             c = tolower(c);
         }
     }
     
-    // Output the result
     cout << s << endl;
     return 0;
 }
