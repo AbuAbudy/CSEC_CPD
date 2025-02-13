6f458336eaf9109d7dd1328d798d220f987702c6
AbuAbudy <masfurudat@gmail.com>
2025-02-13 13:50:59 +0300

    BeautifulMatrix.cpp

--- BeautifulMatrix.cpp
+++ BeautifulMatrix.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main() {
     int matrix[5][5];
     int oneRow, oneCol;
-    
-    // Reading the matrix
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
             cin >> matrix[i][j];
@@ -16,10 +14,7 @@ int main() {
             }
         }
     }
-    
-    // Calculating Manhattan distance to the center (2, 2)
     int moves = abs(oneRow - 2) + abs(oneCol - 2);
-    
     cout << moves << endl;
     return 0;
 }
