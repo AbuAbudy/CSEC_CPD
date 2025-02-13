3e626156f501c28dcbbede8bae01b68df860957c
AbuAbudy <masfurudat@gmail.com>
2025-02-13 13:57:45 +0300

    ShaassandOskols.cpp

--- ShaassandOskols.cpp
+++ ShaassandOskols.cpp
@@ -6,13 +6,12 @@ int main() {
     cin >> n;
     int birds[100];
     for (int i = 0; i < n; ++i) cin >> birds[i];
-    
     int m;
     cin >> m;
     while (m--) {
         int x, y;
         cin >> x >> y;
-        --x; // Convert to 0-based index
+        --x;
         if (x > 0) birds[x - 1] += y - 1;
         if (x < n - 1) birds[x + 1] += birds[x] - y;
         birds[x] = 0;
