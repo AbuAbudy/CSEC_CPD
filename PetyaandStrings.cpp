4ff5b85e4265526a21b582a3e02011e06d17222c
AbuAbudy <masfurudat@gmail.com>
2025-02-13 13:56:14 +0300

    Update PetyaandStrings.cpp

--- PetyaandStrings.cpp
+++ PetyaandStrings.cpp
@@ -2,16 +2,14 @@
 #include <algorithm>
 #include <cctype>
 using namespace std;
- 
+
 int main() {
     string s1, s2;
     cin >> s1 >> s2;
- 
-    // Convert both strings to lowercase
+
     transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
     transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
- 
-    // Compare the strings
+
     if (s1 < s2) {
         cout << "-1" << endl;
     } else if (s1 > s2) {
@@ -19,6 +17,6 @@ int main() {
     } else {
         cout << "0" << endl;
     }
- 
+
     return 0;
 }
